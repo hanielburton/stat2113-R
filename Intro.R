a407db6e14dc9d2d378b7dc01a5a9d5a70791ed4
hanielburton <hanielburton@me.com>
2020-01-21 12:11:08 -0600

    Intro to R

---
+++ Intro.R
@@ -0,0 +1,36 @@
+# January 21, 2020
+# Intro to R
+# By Haniel Burton
+
+
+#### Example R.1 ###
+3 * 19
+
+3 - 16 / 2
+
+5 * pi^2
+
+#log10 is default base 10 log
+5 + log10(1000)
+
+#log is natural log, aka ln
+log(exp(5))
+
+### Example R.2 ###
+# Assignment operator
+
+x <- 7
+
+g <- x^2 - 3*x + 8
+
+f <- 2*g - 5
+
+#arrow is the preferred assignment operator, although the equal sign also works
+#use equal sign for specifying options in functions
+
+
+### Example R.3 ###
+
+#Ccoperator stands for concatenate
+
+gpa <- c(1.9, 3.2, 2.0, 2.9, 2.7, 3.3, 2.8, 3.0, 3.8, 2.7, 2.0, 1.9)
